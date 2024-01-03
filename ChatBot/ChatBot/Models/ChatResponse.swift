1d777aa1376a904ba1a4ae2593ed2c82269c9a7e
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2024-01-03 11:26:03 +0900

    feat: Chat Response 생성

---
+++ ChatBot/ChatBot/Models/ChatResponse.swift
@@ -0,0 +1,48 @@
+//
+//  ChatResponse.swift
+//  ChatBot
+//
+//  Created by 김예준 on 1/3/24.
+//
+
+import Foundation
+
+struct ChatResponse: Decodable {
+    let id: String
+    let object: String
+    let created: Int
+    let model: String
+    let systemFingerprint: String
+    let choices: [Choice]
+    let usage: Usage
+    
+    enum CodingKeys: String, CodingKey {
+        case id, object, created, model, choices, usage
+        case systemFingerprint = "system_fingerprint"
+    }
+}
+
+struct Choice: Decodable {
+    let index: Int
+    let message: Message
+    let logProblems: Int?
+    let finishReason: String
+    
+    enum CodingKeys: String, CodingKey {
+        case index, message
+        case logProblems = "logprobs"
+        case finishReason = "finish_reason"
+    }
+}
+
+struct Usage: Decodable {
+    let promptTokens: Int
+    let completionTokens: Int
+    let totalTokens: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case promptTokens = "prompt_okens"
+        case completionTokens = "completion_tokens"
+        case totalTokens = "total_tokens"
+    }
+}
