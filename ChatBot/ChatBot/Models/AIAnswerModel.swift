aae80efa0d98e885dadb21a736ca6a1da9f348e6
JEON-Sungsu <anagma@naver.com>
2024-01-03 11:35:05 +0900

    feat: Request Model, Response Model 작성, 폴더 정리

---
+++ ChatBot/ChatBot/Models/AIAnswerModel.swift
@@ -0,0 +1,51 @@
+//
+//  AIAnswerModel.swift
+//  ChatBot
+//
+//  Created by 전성수 on 1/3/24.
+//
+
+import Foundation
+
+// MARK: - AIAnswerModel
+struct AIAnswerModel: Decodable {
+    let id, object: String
+    let created: Int
+    let model: String
+    let choices: [Choice]
+    let usage: Usage
+    let systemFingerprint: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, object, created, model, choices, usage
+        case systemFingerprint = "system_fingerprint"
+    }
+}
+
+// MARK: - Choice
+struct Choice: Decodable {
+    let index: Int
+    let message: Message
+    let finishReason: String
+
+    enum CodingKeys: String, CodingKey {
+        case index, message
+        case finishReason = "finish_reason"
+    }
+}
+
+// MARK: - Message
+struct Message: Decodable {
+    let role, content: String
+}
+
+// MARK: - Usage
+struct Usage: Decodable {
+    let promptTokens, completionTokens, totalTokens: Int
+
+    enum CodingKeys: String, CodingKey {
+        case promptTokens = "prompt_tokens"
+        case completionTokens = "completion_tokens"
+        case totalTokens = "total_tokens"
+    }
+}
---
+++ ChatBot/ChatBot/Models/UserQuestionModel.swift
@@ -0,0 +1,25 @@
+//
+//  UserQuestionModel.swift
+//  ChatBot
+//
+//  Created by 전성수 on 1/3/24.
+//
+
+import Foundation
+
+// MARK: - UserQuestionModel
+struct UserQuestionModel: Encodable {
+    let model: String
+    let stream: Bool
+    let userMessage: [UserMessage]
+    
+    enum CodingKeys: String, CodingKey {
+        case model, stream
+        case userMessage = "messages"
+    }
+}
+
+// MARK: - Message
+struct UserMessage: Encodable {
+    let role, content: String
+}
