ce0f141b1243819f722165558650c0da58000966
kimjunsung <assd1108@naver.com>
2024-01-04 14:10:20 +0900

    [feat] configure `HTTPDecoratable` protocol

---
+++ ChatBot/ChatBot/Networks/HTTPRequest/Protocols/HTTPDecoratable.swift
@@ -0,0 +1,12 @@
+//
+//  HTTPDecoratable.swift
+//  ChatBot
+//
+//  Created by 김준성 on 1/4/24.
+//
+
+import Foundation
+
+protocol HTTPDecoratable: HTTPRequestable {
+    var httpRequest: HTTPRequestable { get }
+}
