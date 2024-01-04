af0317f41f749a26c53c2fa5202e5a155f2f7154
kimjunsung <assd1108@naver.com>
2024-01-04 13:35:57 +0900

    [feat] configure `HTTPRequestable` protocol

---
+++ ChatBot/ChatBot/Networks/HTTPRequestable.swift
@@ -0,0 +1,52 @@
+//
+//  HTTPRequestable.swift
+//  ChatBot
+//
+//  Created by 김준성 on 1/4/24.
+//
+
+import Foundation
+
+extension URLRequest {
+    enum HTTPMethod: String {
+        case get = "GET"
+        case post = "POST"
+    }
+}
+
+protocol HTTPRequestable {
+    var paths: [String]? { get }
+    var queryStrings: [String: CustomStringConvertible]? { get }
+    var httpMethod: URLRequest.HTTPMethod { get }
+    
+    func asURLRequest(urlString: String) -> URLRequest?
+}
+
+extension HTTPRequestable {
+    var paths: [String]? { nil }
+    var queryStrings: [String: CustomStringConvertible]? { nil }
+    var httpMethod: URLRequest.HTTPMethod { .get }
+    
+    func asURLRequest(urlString: String) -> URLRequest? {
+        guard var components = URLComponents(string: urlString) else {
+            return nil
+        }
+        
+        if let paths = paths {
+            components.path = "/\(paths.joined(separator: "/"))/"
+        }
+        
+        if let queryStrings = queryStrings {
+            components.queryItems = queryStrings.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
+        }
+        
+        guard let url = components.url else {
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod.rawValue
+        return request
+    }
+    
+}
