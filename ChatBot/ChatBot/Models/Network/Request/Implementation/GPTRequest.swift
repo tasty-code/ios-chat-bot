dd636df9a1cbc795de000cb52a0db85e197ccdf6
JinUng41 <ingjwjw@naver.com>
2024-01-05 13:58:07 +0900

    [Fix] 오타 수정 및 누락된 코드 삽입

--- ChatBot/ChatBot/Models/Network/Request/Implementation/GPTRequest.swift
+++ ChatBot/ChatBot/Models/Network/Request/Implementation/GPTRequest.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 struct GPTRequest: APIRequestable {
-    let baseURL: URL? = URL(string: "")
+    let baseURL: URL? = URL(string: "https://api.openai.com/v1/chat/completions")
     let headerFeilds: [String : String]
     let httpBodyData: Data?
     
-    init?(httpbodyData: Encodable) {
+    init?(httpBodyData: Encodable) {
         guard let apiKey = Bundle.main.gptAPIKey,
-              let encodedData = try? JSONEncoder().encode(httpbodyData)
+              let encodedData = try? JSONEncoder().encode(httpBodyData)
         else {
             return nil
         }
         self.httpBodyData = encodedData
         headerFeilds = [
-            "Authroization": apiKey,
+            "Authorization": "Bearer \(apiKey)",
             "Content-Type" : "application/json"
         ]
     }
