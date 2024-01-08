de25271996baa3865355c943c1342fed465b1301
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2024-01-08 16:17:14 +0900

    feat: api와 networkManager를 사용해 통신
    - 객체를 json으로 encode
    - 받은 json data를 responseModel로 decode

--- ChatBot/ChatBot/ViewController.swift
+++ ChatBot/ChatBot/ViewController.swift
@@ -8,7 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let networkManager = NetworkManager()
+    private let api = ChatAPI()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let response = jsonEncode()
+        guard let request = api.makeRequest(body: response) else {
+            print(NetworkError.invaildURL)
+            return
+        }
+        
+        networkManager.loadData(request: request) { result in
+            switch result {
+            case .success(let data):
+                guard let decodeData = self.jsonDecode(data: data) else { return }
+                print(decodeData)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private func jsonEncode() -> Data? {
+        let encoder = JSONEncoder()
+        let messages = [Message(role: "user", content: "Say this is a test!", toolCalls: nil)]
+        let data = ChatRequestModel(model: "gpt-3.5-turbo", messages: messages, stream: false, logprobs: false)
+        do {
+            let encodeData = try encoder.encode(data)
+            return encodeData
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return nil
+    }
+    
+    private func jsonDecode(data: Data) -> ChatResponseModel? {
+        do {
+            let decodeData = try JSONDecoder().decode(ChatResponseModel.self, from: data)
+            return decodeData
+        } catch {
+            print(error)
+            return nil
+        }
     }
 }
