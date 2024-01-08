fb355e9356369bb6f1cac820a3cb9c5af190960e
imSeongHyeon <jfknfj920@naver.com>
2024-01-08 21:04:19 +0900

    feat: APIKey 불러오기 추가

---
+++ ChatBot/ChatBot/Utilities/Extension/Bundle+Extension.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+extension Bundle {
+    enum APIError: Error, CustomDebugStringConvertible {
+        case noExistedAPIPlist
+        case noExistedAPIKey
+        
+        var debugDescription: String {
+            switch self {
+            case .noExistedAPIPlist: "noExistedAPIPlist"
+            case .noExistedAPIKey: "noExistedAPIKey"
+            }
+        }
+    }
+    
+    private func checkAPIKey() -> Result<String, APIError> {
+        guard let filePath = Bundle.main.path(forResource: "APIKeyList", ofType: "plist") else {
+            return .failure(.noExistedAPIPlist)
+        }
+        
+        if let plist = NSDictionary(contentsOfFile: filePath),
+           let value = plist.object(forKey: "OPENAI_API_KEY") as? String {
+            return .success(value)
+        } else {
+            return .failure(.noExistedAPIKey)
+        }
+    }
+    
+    var APIKey: String {
+        switch checkAPIKey() {
+        case .success(let APIKey):
+            return APIKey
+        case .failure(let error):
+            print(error)
+            return error.localizedDescription
+        }
+    }
+}
