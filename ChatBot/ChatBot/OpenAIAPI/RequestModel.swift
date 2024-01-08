6fd1883c734bcf5cb67917c039403622da02ef45
imSeongHyeon <jfknfj920@naver.com>
2024-01-08 21:03:42 +0900

    refactor: RequestModel 생성시 GPTModel 타입을 받을 수 있도록 수정

--- ChatBot/ChatBot/OpenAIAPI/RequestModel.swift
+++ ChatBot/ChatBot/OpenAIAPI/RequestModel.swift
@@ -1,5 +1,9 @@
 struct RequestModel: Encodable {
-    let model: String
+    let model: GPTModel
     let messages: [Message]
     let stream: Bool?
+}
+
+enum GPTModel: String, Encodable {
+    case gpt_3_5_turbo = "gpt-3.5-turbo"
 }
