7a36d501d7a0bfe63c032be5ed6d99d44e20b6b0
JEON-Sungsu <anagma@naver.com>
2024-01-09 09:59:00 +0900

    feat: 유저 입력값 인코딩 객체 구현

---
+++ ChatBot/ChatBot/Utility/DataTransfer/QuestionEncoder.swift
@@ -0,0 +1,31 @@
+//
+//  QuestionEncoder.swift
+//  ChatBot
+//
+//  Created by 김경록 on 1/8/24.
+//
+
+import Foundation
+
+struct QuestionEncoder {
+    enum Constant {
+        static let model = "gpt-3.5-turbo"
+        static let stream = false
+        static let AIRole = "assistant"
+        static let AIContent = "You are a helpful assistant."
+        static let UserRole = "user"
+    }
+    
+    func transformData(_ question: String) -> Data? {
+        let userMessages: [UserMessage] = [UserMessage(role: Constant.AIRole, content: Constant.AIContent),
+                                           UserMessage(role: Constant.UserRole, content: question)]
+        let userQuestion = UserQuestionModel(model: Constant.model, stream: Constant.stream, userMessage: userMessages)
+        
+        guard let encodedData = try? JSONEncoder().encode(userQuestion) else {
+            print("유저 입력값 인코딩 에러")
+            return nil
+        }
+        
+        return encodedData
+    }
+}
