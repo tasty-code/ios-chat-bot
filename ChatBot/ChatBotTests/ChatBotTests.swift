991c2dca167b75a285745a35b68c7c38a6996e9c
kimjunsung <assd1108@naver.com>
2024-01-10 15:56:03 +0900

    [style] add given, when, then annotation

--- ChatBot/ChatBotTests/ChatBotTests.swift
+++ ChatBot/ChatBotTests/ChatBotTests.swift
@@ -21,12 +21,14 @@ final class ChatBotTests: XCTestCase {
     }
 
     func test_코멘트_요청에_대한_인코딩이_제대로_되는지() throws {
+        // given
         let httpRequest = MockHTTPRequest()
         let expectedModel = JSONFetcher.load(type: Model.GPTReplyDTO.self, fileName: "GPTReplyMock")!
         let encodeObject = Model.GPTCommentDTO(
             messages: [Model.UserMessage(content: "안녕?")]
         )
         
+        // when
         _ = httpService.request(request: httpRequest, object: encodeObject, type: Model.GPTReplyDTO.self)
             .sink(receiveCompletion: { completion in
                 if case let .failure(error) = completion {
@@ -62,6 +64,8 @@ final class ChatBotTests: XCTestCase {
     func test_챗봇_응답에_대한_디코딩이_안될_때_오류를_제대로_만드는지() throws {
         // given
         let httpRequest = MockHTTPRequest()
+        
+        // when
         _ = httpService.request(request: httpRequest, type: Int.self)
             .sink(receiveCompletion: { completion in
                 if case let .failure(error) = completion {
