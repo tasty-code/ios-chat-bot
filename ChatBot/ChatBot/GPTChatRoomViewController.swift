f23fee0221d3c0cc1073a5d4f103d11c9969d174
kimjunsung <assd1108@naver.com>
2024-01-11 14:00:36 +0900

    [config] setting UI configuration to code base

---
+++ ChatBot/ChatBot/GPTChatRoomViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ChatBot
+//
+//  Created by Tacocat on 1/1/24.
+//
+
+import UIKit
+import Combine
+
+class GPTChatRoomViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
