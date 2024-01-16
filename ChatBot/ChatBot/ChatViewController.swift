70da75692f501d1a89087b3ee1f607f353db82ef
comdori-wj <02145s1@gmail.com>
2024-01-16 16:33:20 +0900

    refactor: 컬렉션뷰 리스트 채팅 유저 구현

--- ChatBot/ChatBot/ChatViewController.swift
+++ ChatBot/ChatBot/ChatViewController.swift
@@ -7,43 +7,64 @@
 
 import UIKit
 
+struct ChatMessage: Hashable {
+    let sender: String
+    let message: String
+}
+
 final class ChatViewController: UIViewController {
-        
-    private let textField: UITextField = {
+    private lazy var horizontalStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var textField: UITextField = {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.placeholder = "Type your message here"
-        textField.borderStyle = .roundedRect
-        textField.font = UIFontMetrics.default.scaledFont(for: UIFont.systemFont(ofSize: 16.0))
+        textField.layer.cornerRadius = 16
+        textField.layer.borderWidth = 1.5
+        textField.layer.borderColor = UIColor.red.cgColor
+        textField.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         return textField
     }()
     
-    private let sendButton: UIButton = {
+    private lazy var sendButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Send", for: .normal)
+        button.setTitle("전송", for: .normal)
         button.backgroundColor = .systemMint
-        
+
+        button.layer.cornerRadius = 16
+        button.setContentHuggingPriority(.required, for: .horizontal)
+        button.setContentCompressionResistancePriority(.required, for: .horizontal)
         return button
     }()
     
     typealias ChatDataSource = UICollectionViewDiffableDataSource<Int, ChatMessage>
-    
+    typealias CellRegistration = UICollectionView.CellRegistration<ChatCollectionViewCell, ChatMessage>
+
     private var collectionView: UICollectionView!
-    
     private var dataSource: ChatDataSource!
+    private var cellRegistration: CellRegistration!
     
-    // TODO: 이걸 CoreData로 저장
+    // TODO: 이걸 CoreData로 저장 예정
     private var messageStorage = [ChatMessage]()
-    
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureHirearchy()
+        setUpLayouts()
+        setConstraints()
         configureDataSource()
-        
+        configureCellRegistration()
     }
+    
 }
 
 // MARK: - Configure Layout
@@ -51,22 +72,21 @@ final class ChatViewController: UIViewController {
 extension ChatViewController {
     
     private func createLayout() -> UICollectionViewLayout {
-        let config = UICollectionLayoutListConfiguration(appearance: .plain)
+        var config = UICollectionLayoutListConfiguration(appearance: .plain)
+        config.showsSeparators = false
         return UICollectionViewCompositionalLayout.list(using: config)
     }
-    
-    private func configureHirearchy() {
-        let layout = createLayout()
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+
+    private func setUpLayouts() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .white
+        collectionView.backgroundColor = .systemBackground
         view.backgroundColor = .white
         view.addSubview(collectionView)
-        view.addSubview(textField)
-        view.addSubview(sendButton)
         
-        setConstraints()
+        horizontalStackView.addArrangedSubviews([textField, sendButton])
+        view.addSubview(horizontalStackView)
         sendButton.addTarget(self, action: #selector(sendButtonTapped), for: .touchUpInside)
     }
     
@@ -74,25 +94,16 @@ extension ChatViewController {
         let safeArea = view.safeAreaLayoutGuide
         
         NSLayoutConstraint.activate([
-            
-            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
-            collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             collectionView.bottomAnchor.constraint(equalTo: textField.topAnchor),
-            
-            textField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
-            textField.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -16),
-            textField.widthAnchor.constraint(equalTo: safeArea.widthAnchor, constant: -100),
-            
-            sendButton.leadingAnchor.constraint(equalTo: textField.trailingAnchor, constant: 8),
-            sendButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
-            sendButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -16),
+            sendButton.widthAnchor.constraint(equalToConstant: 35),
+            sendButton.heightAnchor.constraint(equalToConstant: 35),
+            horizontalStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            horizontalStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            horizontalStackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -60)
         ])
-    }
-    
-    struct ChatMessage: Hashable {
-        let sender: String
-        let message: String
     }
     
     @objc
@@ -114,85 +125,15 @@ extension ChatViewController {
 extension ChatViewController {
     
     private func configureDataSource() {
-        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, ChatMessage> { (cell, indexPath, item) in
-            var contentConfiguration = UIListContentConfiguration.cell()
-            let chatMessage = self.messageStorage[indexPath.row]
-            contentConfiguration.text = "\(chatMessage.message)"
-            
-            var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
-//            backgroundConfiguration.customView = self.makeChatBalloonView(cell: cell, role: chatMessage.sender, message: chatMessage.message)
-            let bubbleView = BubbleView(width: 100, height: 50)
-//            bubbleView.sizeToFit()
-            backgroundConfiguration.customView = bubbleView
-            cell.contentConfiguration = contentConfiguration
-            cell.backgroundConfiguration = backgroundConfiguration
-            cell.backgroundView?.sizeToFit()
-        }
-        
-        dataSource = ChatDataSource(collectionView: collectionView) { (collectionView, indexPath, identifier) -> UICollectionViewCell? in
-            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
-                                                                for: indexPath,
-                                                                item: identifier)
+        dataSource = ChatDataSource(collectionView: collectionView) { (collectionView, indexPath, identifier) -> ChatCollectionViewCell? in
+            collectionView.allowsSelection = false
+            return collectionView.dequeueConfiguredReusableCell(using: self.cellRegistration, for: [indexPath.row], item: identifier)
         }
     }
     
-    private func makeChatBalloonView(cell: UICollectionViewListCell, role: String, message: String) -> UIView {
-        let chatBalloon: UIView = {
-            
-            let messageHeight = heightForMessage(message: message, maxWidth: cell.bounds.width - 16) + 20 // Adjust the padding as needed
-            
-            // Set the initial height of the chat balloon
-            let initialHeight: CGFloat = cell.bounds.height // Set your desired initial height
-            let height = max(messageHeight, initialHeight)
-            print("height: \(initialHeight) message: \(message)")
-            
-            let view = UIView(frame: CGRect(x: cell.frame.origin.x, y: cell.frame.origin.y, width: cell.frame.width, height: height))
-            
-            var maskPath: UIBezierPath
-            switch role {
-            case "mine":
-                view.backgroundColor = .systemBlue
-                
-                // Set corner radius for all corners except the top right corner
-                maskPath = UIBezierPath(
-                    roundedRect: view.bounds,
-                    byRoundingCorners: [.topLeft, .bottomLeft, .bottomRight],
-                    cornerRadii: CGSize(width: 10, height: 10)
-                )
-            case "ai":
-                view.backgroundColor = .systemGray
-                
-                // Set corner radius for all corners except the top right corner
-                maskPath = UIBezierPath(
-                    roundedRect: view.bounds,
-                    byRoundingCorners: [.topRight, .bottomLeft, .bottomRight],
-                    cornerRadii: CGSize(width: 10, height: 10)
-                )
-            default:
-                print("enum으로 바꾸기")
-                maskPath = UIBezierPath()
-            }
-            
-            let maskLayer = CAShapeLayer()
-            maskLayer.path = maskPath.cgPath
-            view.layer.mask = maskLayer
-            return view
-        }()
-        
-        return chatBalloon
+    private func configureCellRegistration() {
+        cellRegistration = UICollectionView.CellRegistration<ChatCollectionViewCell, ChatMessage> { (cell, indexPath, item) in
+            cell.item = item
+        }
     }
-    
-    
-    // Function to calculate the height of the message based on the width constraint
-    private func heightForMessage(message: String, maxWidth: CGFloat) -> CGFloat {
-        let font = UIFontMetrics.default.scaledFont(for: UIFont.systemFont(ofSize: 16.0))
-        let size = CGSize(width: maxWidth, height: .greatestFiniteMagnitude)
-        
-        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
-        let attributes = [NSAttributedString.Key.font: font]
-        
-        let boundingRect = message.boundingRect(with: size, options: options, attributes: attributes, context: nil)
-        return ceil(boundingRect.height)
-    }
-    
 }
--- ChatBot/ChatBot/Views/BubbleView.swift
+++ ChatBot/ChatBot/Views/BubbleView.swift
@@ -6,31 +6,24 @@
 //
 import UIKit
 
-class BubbleView: UILabel {
-    
-    private var width: Int!
-    private var height: Int!
-    
-    required init(width: Int, height: Int) {
-        super.init(frame: .zero)
-        
-        self.width = width
-        self.height = height
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    
+final class BubbleView: UIView {
+    var color = UIColor()
+
     override func draw(_ rect: CGRect) {
+        createBubble(color: color)
+    }
+    
+    func createBubble(color: UIColor) {
         // user
-        let bubbleRect = CGRect(x: 5, y: 5, width: width, height: height)
+        let bubbleRect = CGRect(x: 0, y: 0, width: layer.frame.width - 10, height: layer.frame.height)
         let path = UIBezierPath(roundedRect: bubbleRect, byRoundingCorners: [.topLeft, .topRight, .bottomLeft], cornerRadii: CGSize(width: 5, height: 5))
         let plus = CGFloat(10)
         path.move(to: CGPoint(x: bubbleRect.maxX, y: bubbleRect.maxY))
         path.addLine(to: CGPoint(x: bubbleRect.maxX + plus, y: bubbleRect.maxY))
         path.addLine(to: CGPoint(x: bubbleRect.maxX, y: bubbleRect.maxY - plus))
+
+        color.setFill()
+        path.fill()
         path.close()
         
         // gpt
@@ -42,15 +35,5 @@ class BubbleView: UILabel {
         //        path.addLine(to: CGPoint(x: bubbleRect.minX + plus, y: bubbleRect.maxY - plus))
         //        path.close()
         
-        // CAShapeLayer 객체 생성
-        let layer = CAShapeLayer()
-        layer.path = path.cgPath    // CGPath(Core Graphics) 타입으로 전달
-        
-        // 그리기 속성 설정
-        layer.lineWidth = 3
-        layer.fillColor = UIColor.systemGray4.cgColor
-        //        layer.addSublayer(msgLabel)
-        // View의 서브 Layer에 shapeLayer를 추가
-        self.layer.addSublayer(layer)
     }
 }
