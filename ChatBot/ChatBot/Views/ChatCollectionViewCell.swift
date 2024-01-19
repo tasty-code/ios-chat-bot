42222830cbf2b5cfcb04a8fc022f021db423ec9f
DongJun96 <djlee1201@naver.com>
2024-01-19 15:43:29 +0900

    feat: ChatCollectionViewCell 레이아웃 수정

--- ChatBot/ChatBot/Views/ChatCollectionViewCell.swift
+++ ChatBot/ChatBot/Views/ChatCollectionViewCell.swift
@@ -11,15 +11,37 @@ class ChatCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "chat-cell"
     
-    let promptTextView: UITextView = {
-        let view = UITextView()
-        view.font = .systemFont(ofSize: 18.0)
-        view.textColor = .black
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 15.0
-        view.layer.masksToBounds = false // 기본값
-        view.isEditable = false
+    private var chatBotBubbleConstarint: [NSLayoutConstraint] = []
+    private var userBubbleConstraint: [NSLayoutConstraint] = []
+    
+    private lazy var containerView: UIView = {
+        var view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = false
+        view.layer.cornerRadius = 4
+        view.addSubview(chatLabel)
+        view.addSubview(bubbleTail)
+        
         return view
+    }()
+    
+    private lazy var bubbleTail: ChatBubbleTail = {
+        var tail = ChatBubbleTail(frame: CGRect(x: 0, y: 0, width: 15, height: 10))
+        tail.translatesAutoresizingMaskIntoConstraints = false
+        tail.backgroundColor = .clear
+        
+        return tail
+        
+    }()
+    
+    private lazy var chatLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.font = .systemFont(ofSize: 16)
+        label.numberOfLines = 0
+        
+        return label
     }()
     
     override init(frame: CGRect) {
@@ -27,46 +49,75 @@ class ChatCollectionViewCell: UICollectionViewCell {
         setupView()
     }
     
-    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("required init-error")
     }
     
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        promptTextView.text = nil
+    // MARK: - methods
+    private func setupView() {
+        contentView.addSubview(containerView)
         
-    }
-    
-    func setupView() {
-        contentView.addSubview(promptTextView)
-        promptTextView.translatesAutoresizingMaskIntoConstraints = false
-        promptTextView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        promptTextView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            containerView.widthAnchor.constraint(lessThanOrEqualTo: contentView.widthAnchor, multiplier: 0.8),
+            
+            chatLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 10),
+            chatLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -10),
+            chatLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+            chatLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
+            
+            bubbleTail.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            bubbleTail.widthAnchor.constraint(equalToConstant: 15),
+            bubbleTail.heightAnchor.constraint(equalToConstant: 10),
+        ])
     }
     
     func configure(model: ChatViewModel, index: Int) {
-        guard let message = model.getMessage(at: index),
-              message.role != .system,
-              message.role != .tool else { return }
-        
-        promptTextView.text = message.content
-        
-        guard let font = promptTextView.font else { return }
-        let estimateFrame = message.content.getEstimatedFrame(with: font)
-        promptTextView.widthAnchor.constraint(equalToConstant: estimateFrame.width + 16).isActive = true
+        guard let message = model.getMessage(at: index) else { return }
+        let content = message.content
+        chatLabel.text = content
         
         switch message.role {
         case .assistant:
-            promptTextView.backgroundColor = .systemRed
-            promptTextView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 36).isActive = true
-            promptTextView.addTipViewToLeftBottom(with: promptTextView.backgroundColor)
+            configureChatBotBubble()
         case .user:
-            promptTextView.backgroundColor = .systemBlue
-            promptTextView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -36).isActive = true
-            promptTextView.addTipViewToRightBottom(with: promptTextView.backgroundColor)
+            configureUserBubble()
         default:
             break
         }
+    }
+    
+    private func configureChatBotBubble() {
+        chatBotBubbleConstarint = [
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            bubbleTail.trailingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 3)
+        ]
+        containerView.backgroundColor = .darkGray
+        bubbleTail.color = .darkGray
+        bubbleTail.transform = CGAffineTransform(scaleX: -1, y: 1)
+        chatLabel.textColor = .white
+        
+        NSLayoutConstraint.activate(chatBotBubbleConstarint)
+    }
+    
+    private func configureUserBubble() {
+        userBubbleConstraint = [
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            bubbleTail.leadingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: 3)
+        ]
+        containerView.backgroundColor = .systemRed
+        bubbleTail.color = .systemRed
+        bubbleTail.transform = CGAffineTransform(scaleX: 1, y: 1)
+        chatLabel.textColor = .black
+        
+        NSLayoutConstraint.activate(userBubbleConstraint)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        NSLayoutConstraint.deactivate(chatBotBubbleConstarint + userBubbleConstraint)
+        layoutIfNeeded()
     }
 }
