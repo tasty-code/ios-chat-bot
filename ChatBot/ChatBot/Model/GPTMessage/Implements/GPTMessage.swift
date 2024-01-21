bab6d6a5e04e058e574bab016ee1a92dfeee4dde
kimjunsung <assd1108@naver.com>
2024-01-21 21:54:17 +0900

    [refactor] update function `encode` & initializer

--- ChatBot/ChatBot/Model/GPTMessage/Implements/GPTMessage.swift
+++ ChatBot/ChatBot/Model/GPTMessage/Implements/GPTMessage.swift
@@ -9,14 +9,15 @@ import Foundation
 
 extension Model {
     struct GPTMessage: GPTMessagable, Identifiable {
-        let id: UUID = UUID()
+        let id: UUID
         let role: GPTMessageRole
         let content: String?
         let name: String?
         let toolCalls: [GPTToolCall]?
         let toolCallID: String?
         
-        init(role: GPTMessageRole, content: String?, name: String?, toolCalls: [GPTToolCall]?, toolCallID: String?) {
+        init(id: UUID = UUID(), role: GPTMessageRole, content: String?, name: String?, toolCalls: [GPTToolCall]?, toolCallID: String?) {
+            self.id = id
             self.role = role
             self.content = content
             self.name = name
@@ -25,6 +26,7 @@ extension Model {
         }
         
         init(requestMessage: Model.GPTMessage) {
+            self.id = requestMessage.id
             self.role = requestMessage.role
             self.content = requestMessage.content
             self.name = requestMessage.name
@@ -38,11 +40,31 @@ extension Model {
 
 extension Model.GPTMessage: Codable {
     enum CodingKeys: String, CodingKey {
+        case id
         case role
         case content
         case name
         case toolCalls = "tool_calls"
         case toolCallID = "tool_call_id"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = UUID()
+        self.role = try container.decode(Model.GPTMessageRole.self, forKey: .role)
+        self.content = try container.decodeIfPresent(String.self, forKey: .content)
+        self.name = try container.decodeIfPresent(String.self, forKey: .name)
+        self.toolCalls = try container.decodeIfPresent([Model.GPTToolCall].self, forKey: .toolCalls)
+        self.toolCallID = try container.decodeIfPresent(String.self, forKey: .toolCallID)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(role, forKey: .role)
+        try container.encodeIfPresent(content, forKey: .content)
+        try container.encodeIfPresent(name, forKey: .name)
+        try container.encodeIfPresent(toolCalls, forKey: .toolCalls)
+        try container.encodeIfPresent(toolCallID, forKey: .toolCallID)
     }
 }
 
