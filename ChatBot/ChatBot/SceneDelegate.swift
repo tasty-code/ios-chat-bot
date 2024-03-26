ef13b53b521666a6af4d6b5ad88f92587764d732
suojae3 <suojae3@gmail.com>
2024-03-26 17:10:04 +0900

    :wrench: config: Delete storyboard

--- ChatBot/ChatBot/SceneDelegate.swift
+++ ChatBot/ChatBot/SceneDelegate.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) { }
     func sceneDidBecomeActive(_ scene: UIScene) { }
     func sceneWillResignActive(_ scene: UIScene) { }
