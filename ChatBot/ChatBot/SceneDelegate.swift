812dd4bea2d4cca8448850e8fbe3b7e3bf480729
Matthew(매튜) <krsder@naver.com>
2024-03-26 13:22:57 +0900

    config: storyboard 삭제 및 view 세팅

--- ChatBot/ChatBot/SceneDelegate.swift
+++ ChatBot/ChatBot/SceneDelegate.swift
@@ -11,7 +11,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard
+            let windowScene = (scene as? UIWindowScene)
+        else {
+            return
+        }
+        window = UIWindow(windowScene: windowScene)
+        
+        let mainViewController =  ViewController()
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) { }
