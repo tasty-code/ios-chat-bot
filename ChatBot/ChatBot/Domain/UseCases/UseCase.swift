f8a8dd39830a9c8bd7f251527a45f9a25858820b
suojae3 <suojae3@gmail.com>
2024-03-27 11:14:44 +0900

    :sparkles: feat: Add SendChatMessageUseCase

--- ChatBot/ChatBot/Domain/UseCases/UseCase.swift
+++ ChatBot/ChatBot/Domain/UseCases/UseCase.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import RxSwift
+
+struct SendChatMessageUseCase {
+    func execute(model: String, content: String) -> Observable<[ChatMessageEntity]> {
+        return .empty()
+    }
+}
