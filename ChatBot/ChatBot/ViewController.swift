bca5481942f837fd58aa16b215eea1feccc511c5
eensungkim <eensungkim@gmail.com>
2024-03-28 20:18:20 +0900

    fix: 말풍선이 텍스트의 길이에 따라 변화하도록 수정

--- ChatBot/ChatBot/ViewController.swift
+++ ChatBot/ChatBot/ViewController.swift
@@ -31,7 +31,8 @@ class ViewController: UIViewController {
     }
     
     @objc func tapButton() {
-        let text = "안녕하세요! 글이 길더라도 일정한 길이를 유지하면서 방향만 바뀌도록 구현해 보았습니다. 이정도면 이제 충분히 값을 잘 담아줄 수 있는 것 같네요."
+        let text = "안녕하세요!"
+        let text2 = "만약에 이 문장이 엄청나게 길어진다면 어떤 변화를 가져오게 될지 과연 짐작이나 되실런지요."
         
         isUser.toggle()
         chatBubbleView.setUser(isUser)
@@ -46,17 +47,16 @@ extension ViewController {
         
         chatBubbleView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            $0.width.equalTo(view.snp.width).multipliedBy(0.65)
+            $0.width.lessThanOrEqualTo(view.snp.width).multipliedBy(0.7).priority(.high)
             $0.leading.equalTo(view.snp.leading).inset(10)
         }
     }
     
     private func configureChatBubbleViewSide(_ isUser: Bool) {
-        print(isUser)
         chatBubbleView.setUser(isUser)
         chatBubbleView.snp.remakeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            $0.width.equalTo(view.snp.width).multipliedBy(0.65)
+            $0.width.lessThanOrEqualTo(view.snp.width).multipliedBy(0.7).priority(.high)
             if isUser {
                 $0.leading.equalTo(view.snp.leading).inset(10)
             } else {
