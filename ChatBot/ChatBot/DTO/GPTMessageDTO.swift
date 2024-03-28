12dca446eb9b6a4a0d0de678d29322e14a59a247
wlsdud-dev <hjymail1025@gmail.com>
2024-03-28 18:53:19 +0900

    feat: 응답 DTO 구현

--- ChatBot/ChatBot/DTO/GPTMessageDTO.swift
+++ ChatBot/ChatBot/DTO/GPTMessageDTO.swift
@@ -1,8 +1,22 @@
-//
-//  GPTMessageDTO.swift
-//  ChatBot
-//
-//  Created by Jin-Mac on 3/28/24.
-//
-
 import Foundation
+
+// MARK: - Message
+struct Message: Codable {
+    let role: Role
+    let content: String?
+    let toolCalls: [ToolCall]?
+    
+    enum CodingKeys: String, CodingKey {
+            case content, role
+            case toolCalls = "tool_calls"
+    }
+}
+
+struct ToolCall: Codable {
+    let id, type: String
+    let function: Function
+}
+
+struct Function: Codable {
+    let name, arguments: String
+}
--- ChatBot/ChatBot/DTO/GPTResponseDTO.swift
+++ ChatBot/ChatBot/DTO/GPTResponseDTO.swift
@@ -1,8 +1,54 @@
-//
-//  GPTResponseDTO.swift
-//  ChatBot
-//
-//  Created by Jin-Mac on 3/28/24.
-//
-
 import Foundation
+
+struct GPTResponseDTO: Decodable {
+    let id, object: String
+    let created: Int
+    let model: String
+    let choices: [Choice]
+    let usage: Usage
+    let systemFingerprint: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, object, created, model, choices, usage
+        case systemFingerprint = "system_fingerprint"
+    }
+}
+
+struct Choice: Decodable {
+    let index: Int
+    let message: Message
+    let logprobs: Logprobs?
+    let finishReason: String
+
+    enum CodingKeys: String, CodingKey {
+        case index, message, logprobs
+        case finishReason = "finish_reason"
+    }
+}
+
+struct Logprobs: Decodable {
+    let content: [Content]?
+}
+
+struct Content: Decodable {
+    let token: String
+    let logprob: Double
+    let bytes: [Int]?
+    let topLogprobs: [TopLogprobs]
+}
+
+struct TopLogprobs: Decodable {
+    let token: String
+    let logprob: Double
+    let buyes: [Int]?
+}
+
+struct Usage: Decodable {
+    let promptTokens, completionTokens, totalTokens: Int
+
+    enum CodingKeys: String, CodingKey {
+        case promptTokens = "prompt_tokens"
+        case completionTokens = "completion_tokens"
+        case totalTokens = "total_tokens"
+    }
+}
