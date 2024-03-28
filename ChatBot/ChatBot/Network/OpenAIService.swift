e6afed40a7229674385f8b034042da77a5fadc8c
Hoi.Kwon <teaho177@gmail.com>
2024-03-28 22:55:48 +0900

    feat(*): Request 요청및 처리를 위한 OpenAIService 구현

---
+++ ChatBot/ChatBot/Network/OpenAIService.swift
@@ -0,0 +1,44 @@
+//
+//  OpenAIService.swift
+//  ChatBot
+//
+//  Created by 권태호 on 28/03/2024.
+//
+
+import Foundation
+
+class OpenAIService {
+    func sendRequestToOpenAI(_ messages: [RequestMessageModel], completion: @escaping (Result<[Message], Error>) -> Void) {
+        let endpoint = OpenAIEndPoint.chatCompletionsBaseURL
+        var request = URLRequest(url: endpoint.url)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.addValue("Bearer \(APIKeyManager.openAIAPIKey)", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let requestBody: [String: Any] = [
+            "model": "gpt-3.5-turbo",
+            "messages": messages.map { ["role": $0.role.rawValue, "content": $0.content] }
+        ]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: requestBody)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No data", code: 0, userInfo: nil)))
+                return
+            }
+            do {
+                let responseDTO = try JSONDecoder().decode(OpenAICheatResponseDTO.self, from: data)
+                let messages = responseDTO.choices.compactMap { choice -> Message? in
+                    guard let messageContent = choice.message?.content, let messageRole = choice.message?.role else { return nil }
+                    return Message(role: (MessageRole(rawValue: messageRole) ?? .assistant).rawValue, content: messageContent)
+                }
+                completion(.success(messages))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
