1f84119736ea8d5b943397673290160984ee80bc
Hoi.Kwon <teaho177@gmail.com>
2024-03-28 22:46:58 +0900

    feat(*): API 통신을 위한 EndPoint 구현

---
+++ ChatBot/ChatBot/Network/OpenAIEndPoint.swift
@@ -0,0 +1,33 @@
+//
+//  OpenAIEndPoint.swift
+//  ChatBot
+//
+//  Created by 권태호 on 28/03/2024.
+//
+
+import Foundation
+
+struct OpenAIEndPoint {
+    let path: String
+    var url: URL {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.openai.com"
+        components.path = "/" + path
+        
+        guard let url = components.url else {
+            preconditionFailure("URL Error: \(components)")
+        }
+        return url
+    }
+}
+
+extension OpenAIEndPoint {
+    static let chatCompletionsBaseURL = OpenAIEndPoint(path: "v1/chat/completions")
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
