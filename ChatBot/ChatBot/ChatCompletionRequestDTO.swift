5f1b8a259669cce653e28cc6bc62a90a56aa445a
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-03-28 13:31:03 +0900

    chore: request 불필요한 필드 제거

--- ChatBot/ChatBot/ChatCompletionRequestDTO.swift
+++ ChatBot/ChatBot/ChatCompletionRequestDTO.swift
@@ -1,58 +1,12 @@
-import Foundation
-
 struct ChatCompletionRequestDTO {
     let model: String
+    let stream: Bool
     let messages: [Message]
-    let stream: Bool?
-    let maxTokens: Int?
-    let tools: [Tool]?
 }
 
 extension ChatCompletionRequestDTO {
     struct Message {
         let role: String
-        let name: String?
         let content: String
-        let tools: [Tool]?
-        let functionCall: Function?
-        let toolCallId: String?
     }
 }
-extension ChatCompletionRequestDTO {
-    struct Tool {
-        let type: String
-        let function: Function
-    }
-}
-
-extension ChatCompletionRequestDTO {
-    struct Function {
-        let name: String
-        let description: String
-        let parmeters: Parameter
-    }
-}
-
-extension ChatCompletionRequestDTO.Function {
-    struct Parameter {
-        let location: Location
-        let unit: Unit
-    }
-}
-
-extension ChatCompletionRequestDTO.Function.Parameter {
-    struct Location {
-        let type: String
-        let description: String
-    }
-}
-
-
-extension ChatCompletionRequestDTO.Tool {
-    struct Function {
-        let name: String
-        let arguments: String
-    }
-}
-
-
