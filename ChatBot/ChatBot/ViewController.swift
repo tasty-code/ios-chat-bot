50d77468f69ad7414bd2d2a0f3f85b7343b55f7c
hemil0102 <hemil0102@gmail.com>
2024-03-28 20:07:22 +0900

    feat: response확인용 loadData() 구현 #1

--- ChatBot/ChatBot/ViewController.swift
+++ ChatBot/ChatBot/ViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let jsonDecoder: JsonDecodableProtocol = JsonDecoder(jsonDecoder: JSONDecoder())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
+    }
+    
+    private func loadData() {
+        let result: Result<GPTResponseDTO, JsonError> = jsonDecoder.decode(fileName: "responseData", fileType: "json")
+        switch result {
+        case .success(let data):
+            print(data)
+        case .failure(let error):
+            print(error)
+        }
     }
 }
