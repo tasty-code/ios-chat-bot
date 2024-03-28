2c5bb811ac0dd229c4129791af094715cd6f22a3
wlsdud-dev <hjymail1025@gmail.com>
2024-03-28 20:35:16 +0900

    test: requst 인코딩 테스트용 기능 구현 #1

--- ChatBot/ChatBot/ViewController.swift
+++ ChatBot/ChatBot/ViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 class ViewController: UIViewController {
     
     let jsonDecoder: JsonDecodableProtocol = JsonDecoder(jsonDecoder: JSONDecoder())
+    let jsonEncoder: JsonEncodableProtocol = JsonEncoder(jsonEncoder: JSONEncoder())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
+        loadEncodingData()
+    }
+    
+    private func makeHTTPBody() -> Result<Data?, JsonError> {
+        let file = GPTRequestDTO(model: .basic, messages: [Message(role: .system, content: "You are an assistant that occasionally misspells words", toolCalls: nil), Message(role: .user, content: "Tell me a story.", toolCalls: nil)], logprobs: nil)
+        
+        return jsonEncoder.encode(of: file)
+    }
+    
+    private func loadEncodingData() {
+        print("---------")
+        let result = makeHTTPBody()
+        switch result {
+        case .success(let data):
+            guard let unwrappedData = data else { return }
+            print(String(data:unwrappedData, encoding: .utf8) ?? "")
+        case .failure(let error):
+            print(error)
+        }
     }
     
     private func loadData() {
