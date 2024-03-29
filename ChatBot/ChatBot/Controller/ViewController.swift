e34bb12bddbeab41ef5c9aa21a8c962a32dfc309
Matthew(매튜) <krsder@naver.com>
2024-03-29 14:23:53 +0900

    feat: 메시지 보내고, 응답 기능 구현

--- ChatBot/ChatBot/Controller/ViewController.swift
+++ ChatBot/ChatBot/Controller/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let chatManager: ChatManager?
+    private let chatManager: ChatManager?
     
     init(chatManager: ChatManager?) {
         self.chatManager = chatManager
@@ -18,9 +18,36 @@ class ViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+}
+
+// MARK: - Life Cycle
+extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureBackground()
+        chatGPT(userMessage: "집에 가고싶어욧..")
+    }
+}
+
+// MARK: - Private Method
+private extension ViewController {
+    func configureBackground() {
         view.backgroundColor = .white
+    }
+    
+    func chatGPT(userMessage: String) {
+        Task{
+            do {
+                guard 
+                    let result = try await chatManager?.requestChatResultData(userMessage: userMessage)
+                else {
+                    return
+                }
+                print("질문 : \(userMessage)")
+                print("결과 : \(result.choices[0].message.content)")
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
