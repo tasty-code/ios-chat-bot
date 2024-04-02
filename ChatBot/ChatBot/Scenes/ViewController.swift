633f32f6a48fe91c26f2afd4953b6c7969e6de4b
eunjuchoi <eunjuzzoo@naver.com>
2024-04-02 10:18:07 +0900

    feat: api 호출 테스트

--- ChatBot/ChatBot/Scenes/ViewController.swift
+++ ChatBot/ChatBot/Scenes/ViewController.swift
@@ -7,8 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import RxSwift
+
+final class ViewController: UIViewController {
+    
+    private let service = ChatAPIService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        service.createChat(systemContent: "Hello! How can I assist you today?", 
+                           userContent: "Hello!")
+            .subscribe(onSuccess: { result in
+                print(result)
+            }, onFailure: { error in
+                print(error)
+            })
     }
 }
