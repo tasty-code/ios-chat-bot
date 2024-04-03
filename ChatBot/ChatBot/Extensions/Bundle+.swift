b11c2165ebd9602ef5263e155f326302a3ca906b
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-03 21:26:43 +0900

    feat: Bundle 및 URLSession Extension 구현

---
+++ ChatBot/ChatBot/Extensions/Bundle+.swift
@@ -0,0 +1,19 @@
+//
+//  Bundle+.swift
+//  ChatBot
+//
+//  Created by 강창현 on 4/3/24.
+//
+
+import Foundation
+
+extension Bundle {
+    var chatBotAPIKey: String {
+        guard 
+            let key = self.object(forInfoDictionaryKey: "ChatBot_API_KEY") as? String
+        else {
+            return ""
+        }
+        return key
+    }
+}
