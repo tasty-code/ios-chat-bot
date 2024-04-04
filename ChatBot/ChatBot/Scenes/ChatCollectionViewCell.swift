35b379427bccaddae6f62223cf564ba10ef6b506
eensungkim <eensungkim@gmail.com>
2024-04-04 16:10:13 +0900

    fix: User 입력이 긴 경우 화면 전체를 채우는 이슈 수정

--- ChatBot/ChatBot/Scenes/ChatCollectionViewCell.swift
+++ ChatBot/ChatBot/Scenes/ChatCollectionViewCell.swift
@@ -43,13 +43,8 @@ extension ChatCollectionViewCell {
         chatBubbleView.snp.remakeConstraints {
             $0.top.equalTo(self.snp.top)
             $0.bottom.equalTo(self.snp.bottom)
-            if isUser {
-                $0.trailing.equalTo(self.snp.trailing)
-                $0.leading.greaterThanOrEqualTo(self.snp.leading).multipliedBy(0.75)
-            } else {
-                $0.leading.equalTo(self.snp.leading)
-                $0.trailing.lessThanOrEqualTo(self.snp.trailing).multipliedBy(0.75)
-            }
+            $0.width.lessThanOrEqualTo(self.snp.width).multipliedBy(0.70)
+            _ = isUser ? $0.trailing.equalTo(self.snp.trailing) : $0.leading.equalTo(self.snp.leading)
         }
     }
 }
