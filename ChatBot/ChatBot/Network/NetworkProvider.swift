801a84676b40d2a7aa7beef67303671561883ff6
LeeSe0ngYe0n <goblin__@naver.com>
2024-04-04 21:33:26 +0900

    refactor: Network 생성 리팩토링

--- ChatBot/ChatBot/Network/NetworkProvider.swift
+++ ChatBot/ChatBot/Network/NetworkProvider.swift
@@ -7,15 +7,9 @@
 
 import Foundation
 
-//final class NetworkProvider {
-//    private let urlRequest: URLRequest
-//    
-//    init(type: APIType, chat: RequestChatDTO, userMessage: String) {
-//        self.urlRequest = NetworkURL.makeURLRequest(type: type, chat: chat)!
-//    }
-//    
-//    func makeChatNetwork() -> ChatBotNetwork {
-//        let network = Network<ResponseChatDTO>(urlRequest)
-//        return ChatBotNetwork(network: network)
-//    }
-//}
+final class NetworkProvider {
+    func makeChatNetwork() -> ChatBotNetwork {
+        let network = Network<ResponseChatDTO>()
+        return ChatBotNetwork(network: network)
+    }
+}
