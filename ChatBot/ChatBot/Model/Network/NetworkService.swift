cd85d82ac5fd87b13c6c28ad092edb89b7dbc832
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-04 14:04:32 +0900

    refine: URL Request 생성 책임 분리

--- ChatBot/ChatBot/Model/Network/NetworkService.swift
+++ ChatBot/ChatBot/Model/Network/NetworkService.swift
@@ -21,7 +21,7 @@ struct NetworkService {
         httpMethod: HttpMethod<T>
     ) -> AnyPublisher<ResponseModel, NetworkError> {
         do {
-            let urlRequest = try makeURLRequest(type: type, httpMethod: httpMethod)
+            let urlRequest = try NetworkURL.makeURLRequest(type: type, httpMethod: httpMethod)
             return session.dataTaskPublisher(for: urlRequest)
                 .tryMap { data -> ResponseModel in
                     try JSONHandler.handleDecodedData(data: data)
@@ -33,33 +33,5 @@ struct NetworkService {
         } catch {
             return Fail(error: NetworkError.requestFailError).eraseToAnyPublisher()
         }
-    }
-    
-    private func makeURL(type: APIType) throws -> URL {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = type.host
-        urlComponents.path = type.path
-        
-        guard
-            let url = urlComponents.url
-        else {
-            throw NetworkError.invalidURLError
-        }
-        return url
-    }
-    
-    private func makeURLRequest<T: Encodable>(
-        type: APIType,
-        httpMethod: HttpMethod<T>
-    ) throws -> URLRequest {
-        let url = try makeURL(type: type)
-        var urlRequest = URLRequest(url: url)
-        let (method, body) = httpMethod.type
-        urlRequest.httpMethod = method
-        urlRequest.allHTTPHeaderFields = type.header
-        guard let body else { return urlRequest }
-        urlRequest.httpBody = try JSONHandler.handleEncodedData(data: body)
-        return urlRequest
     }
 }
