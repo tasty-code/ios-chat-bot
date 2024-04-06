76d7e69e2f0ffa1e8eb32ad0449b89a573216d5c
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-06 15:39:31 +0900

    refine: MockData 파싱으로 변경

--- ChatBot/ChatBot/Model/Network/Mock/MockURLSession.swift
+++ ChatBot/ChatBot/Model/Network/Mock/MockURLSession.swift
@@ -11,13 +11,39 @@ import Combine
 final class MockURLSession: URLSessionProtocol {
     typealias Response = AnyPublisher<(Data, URLResponse), NetworkError>
     
-    private let response: Response
+  private var response: Response? = {
+    let data: Data = JSONHandler.load(fileName: "MockData")!
+    let httpResponse = HTTPURLResponse(
+        url: URL(string: "www.naver.com")!,
+        statusCode: 200,
+        httpVersion: nil,
+        headerFields: nil
+    )! as URLResponse
+    let response = Just((data, httpResponse))
+        .setFailureType(to: NetworkError.self)
+        .eraseToAnyPublisher()
+    return response
+  }()
     
-    init(response: Response) {
-        self.response = response
+    init(response: Response? = nil) {
+      self.response = response
     }
-    
+  
+  func makeMockResponse(fileName: String, statusCode: Int) -> Response {
+          let data: Data = JSONHandler.load(fileName: fileName)!
+          let httpResponse = HTTPURLResponse(
+              url: URL(string: "www.naver.com")!,
+              statusCode: statusCode,
+              httpVersion: nil,
+              headerFields: nil
+          )! as URLResponse
+      let response = Just((data, httpResponse))
+          .setFailureType(to: NetworkError.self)
+          .eraseToAnyPublisher()
+      return response
+  }
     func dataTaskPublisher(for request: URLRequest) -> AnyPublisher<Data, NetworkError> {
+      guard let response else { return Empty().eraseToAnyPublisher() }
         return response
             .tryMap { (data: Data, response: URLResponse) in
                 try self.handleHTTPResponse(data: data, httpResponse: response)
--- ChatBot/ChatBot/Model/Network/NetworkTestable.swift
+++ ChatBot/ChatBot/Model/Network/NetworkTestable.swift
@@ -10,7 +10,7 @@ import Combine
 
 protocol NetworkTestable {
     func setMockSession(session: URLSessionProtocol) -> NetworkService
-    func makeMockURLSession(fileName: String, statusCode: Int) throws -> MockURLSession
+    func makeMockURLSession(fileName: String, statusCode: Int) throws -> URLSessionProtocol
 }
 
 extension NetworkTestable {
@@ -18,7 +18,7 @@ extension NetworkTestable {
         return NetworkService(session: session)
     }
     
-    func makeMockURLSession(fileName: String, statusCode: Int) throws -> MockURLSession {
+    func makeMockURLSession(fileName: String, statusCode: Int) throws -> URLSessionProtocol {
             let data: Data = JSONHandler.load(fileName: fileName)!
             let httpResponse = HTTPURLResponse(
                 url: URL(string: "www.naver.com")!,
