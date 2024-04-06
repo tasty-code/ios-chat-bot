9354718bcc2457dd3b9082f1ea12124b5724d698
Paul Lee <earths-voluble.0g@icloud.com>
2024-04-06 20:55:04 +0900

    chore: JSONHandler 에러 처리 수정

--- ChatBot/ChatBot/Model/Network/JSON/JSONHandler.swift
+++ ChatBot/ChatBot/Model/Network/JSON/JSONHandler.swift
@@ -8,48 +8,48 @@
 import Foundation
 
 enum JSONHandler {
-    private static let encoder = JSONEncoder()
-    private static let decoder = JSONDecoder()
-    
-    static func handleEncodedData(data: Encodable) throws -> Data {
-        do {
-            let encodedData = try encoder.encode(data)
-            return encodedData
-        } catch {
-            throw error
-        }
+  private static let encoder = JSONEncoder()
+  private static let decoder = JSONDecoder()
+  
+  static func handleEncodedData(data: Encodable) throws -> Data {
+    do {
+      let encodedData = try encoder.encode(data)
+      return encodedData
+    } catch {
+      throw NetworkError.encodingError
+    }
+  }
+  
+  static func handleDecodedData<T: Decodable>(data: Data?) throws -> T {
+    guard
+      let data = data
+    else {
+      throw NetworkError.noDataError
     }
     
-    static func handleDecodedData<T: Decodable>(data: Data?) throws -> T {
-        guard
-            let data = data
-        else {
-            throw NetworkError.noDataError
-        }
-        
-        do {
-            let decodedData = try decoder.decode(T.self, from: data)
-            return decodedData
-        } catch {
-            throw NetworkError.decodingError
-        }
+    do {
+      let decodedData = try decoder.decode(T.self, from: data)
+      return decodedData
+    } catch {
+      throw NetworkError.decodingError
+    }
+  }
+  
+  static func load(fileName: String) -> Data? {
+    let extensionType = "json"
+    guard
+      let fileLocation = Bundle.main.url(forResource: fileName, withExtension: extensionType)
+    else {
+      print("Failed to get file location for \(fileName).json")
+      return nil
     }
     
-    static func load(fileName: String) -> Data? {
-        let extensionType = "json"
-        guard
-            let fileLocation = Bundle.main.url(forResource: fileName, withExtension: extensionType)
-        else {
-            print("Failed to get file location for \(fileName).json")
-            return nil
-        }
-        
-        do {
-            let data = try Data(contentsOf: fileLocation)
-            return data
-        } catch {
-            print("Failed to load data from \(fileLocation.path): \(error.localizedDescription)")
-            return nil
-        }
+    do {
+      let data = try Data(contentsOf: fileLocation)
+      return data
+    } catch {
+      print("Failed to load data from \(fileLocation.path): \(error.localizedDescription)")
+      return nil
     }
+  }
 }
