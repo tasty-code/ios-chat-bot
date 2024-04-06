e21c6ef42a8a23c1b0e9587e39cbd5caaa41fac4
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-06 15:38:47 +0900

    add: CollectionView 구성 파일 추가

---
+++ ChatBot/ChatBot/View/ChatCell.swift
@@ -0,0 +1,8 @@
+//
+//  ChatCell.swift
+//  ChatBot
+//
+//  Created by 강창현 on 4/5/24.
+//
+
+import Foundation
---
+++ ChatBot/ChatBot/View/ChatCollectionViewDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  ChatCollectionViewDataSource.swift
+//  ChatBot
+//
+//  Created by 강창현 on 4/5/24.
+//
+
+import Foundation
