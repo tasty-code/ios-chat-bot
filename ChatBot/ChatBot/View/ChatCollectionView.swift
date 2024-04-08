ad47d2d7dcce2341588ffe419f66a888cd2ea375
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-08 19:30:23 +0900

    feat: 자동 스크롤 기능 구현

--- ChatBot/ChatBot/View/ChatCollectionView.swift
+++ ChatBot/ChatBot/View/ChatCollectionView.swift
@@ -23,4 +23,13 @@ extension ChatCollectionView {
   func setupChatCell() {
     self.register(ChatCell.self, forCellWithReuseIdentifier: ChatCell.identifier)
   }
+  
+  func srollToBottom() {
+    guard self.numberOfSections > 0 else { return }
+    let indexPath = IndexPath(
+      item: self.numberOfItems(inSection: numberOfSections - 1) - 1,
+      section: numberOfSections - 1
+    )
+    self.scrollToItem(at: indexPath, at: .bottom, animated: true)
+  }
 }
