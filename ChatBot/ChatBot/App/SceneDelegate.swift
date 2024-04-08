02e0051454bfc18692bcb9035a9c51b122a166d6
eensungkim <eensungkim@gmail.com>
2024-04-08 17:16:59 +0900

    feat: ChatViewController 연결

--- ChatBot/ChatBot/App/SceneDelegate.swift
+++ ChatBot/ChatBot/App/SceneDelegate.swift
@@ -13,8 +13,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        window?.rootViewController = viewController
+        let chatViewController = ChatViewController()
+        window?.rootViewController = chatViewController
         window?.makeKeyAndVisible()
     }
 
