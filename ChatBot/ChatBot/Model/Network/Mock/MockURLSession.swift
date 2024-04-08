d7b6bdd520a8ec6a4c6ff4c92554dfde0b320d6a
Changhyun Kang <changhyun.kyle@gmail.com>
2024-04-08 14:09:18 +0900

    refactor: 테스트 로직 리팩토링

--- ChatBot/ChatBot/Model/Network/Mock/MockURLSession.swift
+++ ChatBot/ChatBot/Model/Network/Mock/MockURLSession.swift
@@ -9,22 +9,45 @@ import Foundation
 import Combine
 
 final class MockURLSession: URLSessionProtocol {
-    typealias Response = AnyPublisher<(Data, URLResponse), NetworkError>
-    
-    private let response: Response
-    
-    init(response: Response) {
-        self.response = response
+  typealias Response = AnyPublisher<(Data, URLResponse), NetworkError>
+  
+  private var response: Response?
+  
+  init(statusCode: Int) {
+    self.makeResponse(statusCode: statusCode)
+  }
+  
+  private func makeResponse(statusCode: Int) {
+    let data: Data = JSONHandler.load(fileName: "MockData")!
+    let httpResponse = HTTPURLResponse(
+      url: URL(string: "www.naver.com")!,
+      statusCode: statusCode,
+      httpVersion: nil,
+      headerFields: nil
+    )! as URLResponse
+    let response = Just((data, httpResponse))
+      .setFailureType(to: NetworkError.self)
+      .eraseToAnyPublisher()
+    self.response = response
+  }
+  
+  func dataTaskPublisher<T: Decodable>(for request: URLRequest) -> AnyPublisher<T, NetworkError> {
+    guard
+      let response
+    else {
+      return Fail(error: NetworkError.unknownError).eraseToAnyPublisher()
     }
-    
-    func dataTaskPublisher(for request: URLRequest) -> AnyPublisher<Data, NetworkError> {
-        return response
-            .tryMap { (data: Data, response: URLResponse) in
-                try self.handleHTTPResponse(data: data, httpResponse: response)
-            }
-            .mapError { error in
-                error as? NetworkError ?? NetworkError.networkError(error)
-            }
-            .eraseToAnyPublisher()
-    }
+    return response
+      .tryMap { (data: Data, response: URLResponse) in
+        try self.handleHTTPResponse(data: data, httpResponse: response)
+      }
+      .mapError { error in
+        error as? NetworkError ?? NetworkError.networkError(error)
+      }
+      .decode(type: T.self, decoder: JSONHandler.decoder)
+      .mapError { error in
+        error as? NetworkError ?? NetworkError.decodingError
+      }
+      .eraseToAnyPublisher()
+  }
 }
