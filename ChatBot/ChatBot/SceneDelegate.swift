72b1631f8852c4c4465404962a3e53c0a4bc0bf6
aladdin <33091784+junbangg@users.noreply.github.com>
2024-04-08 15:32:03 +0900

    Revert "AI 챗봇 앱 [Step 1] 이안, 홍 "

--- ChatBot/ChatBot/SceneDelegate.swift
+++ ChatBot/ChatBot/SceneDelegate.swift
@@ -9,17 +9,11 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        // TODO: - rootViewController 설정
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
-        window.makeKeyAndVisible()
-        self.window = window
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
-    
+
     func sceneDidDisconnect(_ scene: UIScene) { }
     func sceneDidBecomeActive(_ scene: UIScene) { }
     func sceneWillResignActive(_ scene: UIScene) { }
--- ChatBot/ChatBot/ViewController.swift
+++ ChatBot/ChatBot/ViewController.swift
@@ -10,7 +10,5 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .green
     }
 }
