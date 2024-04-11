b353ade274933924174f451bee7005d3cc54556a
Paul Lee <earths-voluble.0g@icloud.com>
2024-04-11 10:34:09 +0900

    chore: UITextViewDelegate 수정

--- ChatBot/ChatBot/View/ChatInputTextView.swift
+++ ChatBot/ChatBot/View/ChatInputTextView.swift
@@ -7,27 +7,21 @@
 
 import UIKit
 
-final class ChatInputTextView: UITextView, UITextViewDelegate {
+final class ChatInputTextView: UITextView {
   override init(frame: CGRect, textContainer: NSTextContainer?) {
     super.init(frame: frame, textContainer: textContainer)
-    self.font = .systemFont(ofSize: 20)
+    self.font = .systemFont(ofSize: 17)
     self.layer.borderWidth = 2
-    self.layer.cornerRadius = 20
+    self.layer.cornerRadius = 17
     self.layer.borderColor = CGColor(red: 0.1, green: 0.1, blue: 0.5, alpha: 1)
     self.isScrollEnabled = false
+    self.textContainerInset.left = 10
+    self.textContainerInset.right = 10
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  func textViewDidChange(_ textView: UITextView) {
-    let size = CGSize(width: self.frame.width, height: .infinity)
-    let estimatedSize = textView.sizeThatFits(size)
-    textView.constraints.forEach { constraints in
-      if constraints.firstAttribute == .height {
-        constraints.constant = estimatedSize.height
-      }
-    }
-  }
+
 }
--- ChatBot/ChatBot/View/ChatInputView.swift
+++ ChatBot/ChatBot/View/ChatInputView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ChatInputView: UIView {
+final class ChatInputView: UIView, UITextViewDelegate {
   private let chatInputTextView = ChatInputTextView()
   private let chatSendButton = ChatInputSendButton()
   
@@ -18,6 +18,16 @@ final class ChatInputView: UIView {
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func textViewDidChange(_ textView: ChatInputTextView) {
+    let size = CGSize(width: self.frame.width, height: .infinity)
+    let estimatedSize = textView.sizeThatFits(size)
+    textView.constraints.forEach { constraints in
+      if constraints.firstAttribute == .height {
+        constraints.constant = estimatedSize.height
+      }
+    }
   }
   
   private func configureUI() {
@@ -36,8 +46,8 @@ final class ChatInputView: UIView {
       chatInputTextView.topAnchor.constraint(equalTo: self.topAnchor),
       chatInputTextView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
       
-      chatSendButton.widthAnchor.constraint(equalToConstant: 45),
-      chatSendButton.heightAnchor.constraint(equalToConstant: 45),
+      chatSendButton.widthAnchor.constraint(equalToConstant: 40),
+      chatSendButton.heightAnchor.constraint(equalToConstant: 40),
       chatSendButton.centerYAnchor.constraint(equalTo: chatInputTextView.centerYAnchor),
       chatSendButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15)
     ])
