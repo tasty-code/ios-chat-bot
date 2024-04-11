661714b880b4696d0e10fa246252d0de3e06b646
happykwon <teaho177@gmail.com>
2024-04-11 15:32:14 +0900

    feat(*): 채팅화면 messageCollectionView 구현
    가변적인 cell높이를 구현하기 위해 UICollectionViewFlowLayout 적용

---
+++ ChatBot/ChatBot/View/ChatMessageCollectionView.swift
@@ -0,0 +1,33 @@
+//
+//  ChatMessageCollectionView.swift
+//  ChatBot
+//
+//  Created by 권태호 on 4/11/24.
+//
+
+import UIKit
+
+class ChatMessageCollectionView: UICollectionView {
+    init() {
+          let layout = UICollectionViewFlowLayout()
+          layout.scrollDirection = .vertical
+          super.init(frame: .zero, collectionViewLayout: layout)
+          
+          self.register(MessageCollectionViewCell.self, forCellWithReuseIdentifier: MessageCollectionViewCell.identifier)
+          self.translatesAutoresizingMaskIntoConstraints = false
+          self.backgroundColor = .systemBackground
+          
+          layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+          layout.minimumLineSpacing = 5
+      }
+      
+      required init?(coder: NSCoder) {
+          fatalError("init(coder:) has not been implemented")
+      }
+      
+      override func layoutSubviews() {
+          super.layoutSubviews()
+          guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return }
+          flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10) // 상, 하, 좌, 우 여백 설정
+      }
+}
