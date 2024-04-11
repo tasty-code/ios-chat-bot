bcac7c8df584edc973c065118c85b88843385e33
eensungkim <eensungkim@gmail.com>
2024-04-11 17:09:59 +0900

    refactor: UserChatBubbleView, AssistantChatBubbleView 분리

--- ChatBot/ChatBot/Scenes/ChatBubbleView.swift
+++ ChatBot/ChatBot/Scenes/ChatBubbleView.swift
@@ -11,15 +11,12 @@ import SnapKit
 import Then
 
 /// 말풍선 View
-final class ChatBubbleView: UIView {
+class ChatBubbleView: UIView {
     private(set) var textLabel = UILabel().then {
         $0.numberOfLines = 0
         $0.lineBreakMode = .byWordWrapping
     }
-    
-    private var isUser: Bool = false
-    
-    private var bubbleViewColor: CGColor = UIColor.systemGray5.cgColor
+    private(set) var bubbleBackgroundColor: CGColor = UIColor.systemGray5.cgColor
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,36 +26,6 @@ final class ChatBubbleView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func draw(_ rect: CGRect) {
-        let context = UIGraphicsGetCurrentContext()
-        
-        let padding: CGFloat = 10
-        let startX: CGFloat = rect.minX + padding
-        let startY: CGFloat = rect.minY + padding
-        let endX: CGFloat = rect.maxX - padding
-        let endY: CGFloat = rect.maxY - padding
-        
-        let bubbleWidth: CGFloat = endX - startX
-        let bubbleHeight: CGFloat = endY - startY
-        
-        let messageBody = CGRect(x: startX, y: startY, width: bubbleWidth, height: bubbleHeight)
-        
-        context?.addPath(CGPath(roundedRect: messageBody, cornerWidth: 10, cornerHeight: 10, transform: nil))
-        context?.setFillColor(bubbleViewColor)
-        context?.fillPath()
-        
-        let tailStartPoint = CGPoint(x: isUser ? endX : startX, y: endY - 25)
-        let tailTipPoint = CGPoint(x: isUser ? endX + 10 : startX - 10, y: endY - 15)
-        let tailEndPoint = CGPoint(x: isUser ? endX : startX, y: endY - 15)
-        let tailControlPoint = CGPoint(x: isUser ? endX + 3 : startX - 3, y: endY - 13)
-        
-        context?.move(to: tailStartPoint)
-        context?.addQuadCurve(to: tailTipPoint, control: tailControlPoint)
-        context?.addQuadCurve(to: tailEndPoint, control: tailControlPoint)
-        context?.setFillColor(bubbleViewColor)
-        context?.fillPath()
     }
 }
 
@@ -79,28 +46,95 @@ extension ChatBubbleView {
             ))
         }
     }
-    
-    private func configureBubble(to isUser: Bool) {
-        switch isUser {
-        case true:
-            bubbleViewColor = UIColor.systemBlue.cgColor
-            textLabel.textColor = .white
-        case false:
-            bubbleViewColor = UIColor.systemGray5.cgColor
-            textLabel.textColor = .black
-        }
-    }
 }
 
 // MARK: - Public Methods
 extension ChatBubbleView {
-    func setUser(_ isUser: Bool) {
-        self.isUser = isUser
-        
-        configureBubble(to: isUser)
-    }
-    
     func setText(_ text: String) {
         textLabel.text = text
+    }
+    
+    func setBubbleBackgroundColor(_ color: CGColor) {
+        bubbleBackgroundColor = color
+    }
+    
+    func setTextColor(_ color: UIColor) {
+        textLabel.textColor = color
+    }
+}
+
+/// UserChatBubbleView
+final class UserChatBubbleView: ChatBubbleView {
+    override func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        
+        let padding: CGFloat = 10
+        let startX: CGFloat = rect.minX + padding
+        let startY: CGFloat = rect.minY + padding
+        let endX: CGFloat = rect.maxX - padding
+        let endY: CGFloat = rect.maxY - padding
+        
+        let bubbleWidth: CGFloat = endX - startX
+        let bubbleHeight: CGFloat = endY - startY
+        
+        let messageBody = CGRect(x: startX, y: startY, width: bubbleWidth, height: bubbleHeight)
+        
+        context?.addPath(CGPath(roundedRect: messageBody, cornerWidth: 10, cornerHeight: 10, transform: nil))
+        context?.setFillColor(bubbleBackgroundColor)
+        context?.fillPath()
+        
+        let tailStartPoint = CGPoint(x: endX, y: endY - 25)
+        let tailTipPoint = CGPoint(x: endX + 10, y: endY - 15)
+        let tailEndPoint = CGPoint(x: endX, y: endY - 15)
+        let tailControlPoint = CGPoint(x: endX + 3, y: endY - 13)
+        
+        context?.move(to: tailStartPoint)
+        context?.addQuadCurve(to: tailTipPoint, control: tailControlPoint)
+        context?.addQuadCurve(to: tailEndPoint, control: tailControlPoint)
+        context?.setFillColor(bubbleBackgroundColor)
+        context?.fillPath()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        textLabel.textColor = .white
+        setBubbleBackgroundColor(UIColor.systemBlue.cgColor)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+/// AssistantChatBubbleView
+final class AssistantChatBubbleView: ChatBubbleView {
+    override func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        
+        let padding: CGFloat = 10
+        let startX: CGFloat = rect.minX + padding
+        let startY: CGFloat = rect.minY + padding
+        let endX: CGFloat = rect.maxX - padding
+        let endY: CGFloat = rect.maxY - padding
+        
+        let bubbleWidth: CGFloat = endX - startX
+        let bubbleHeight: CGFloat = endY - startY
+        
+        let messageBody = CGRect(x: startX, y: startY, width: bubbleWidth, height: bubbleHeight)
+        
+        context?.addPath(CGPath(roundedRect: messageBody, cornerWidth: 10, cornerHeight: 10, transform: nil))
+        context?.setFillColor(bubbleBackgroundColor)
+        context?.fillPath()
+        
+        let tailStartPoint = CGPoint(x: startX, y: endY - 25)
+        let tailTipPoint = CGPoint(x: startX - 10, y: endY - 15)
+        let tailEndPoint = CGPoint(x: startX, y: endY - 15)
+        let tailControlPoint = CGPoint(x: startX - 3, y: endY - 13)
+        
+        context?.move(to: tailStartPoint)
+        context?.addQuadCurve(to: tailTipPoint, control: tailControlPoint)
+        context?.addQuadCurve(to: tailEndPoint, control: tailControlPoint)
+        context?.setFillColor(bubbleBackgroundColor)
+        context?.fillPath()
     }
 }
